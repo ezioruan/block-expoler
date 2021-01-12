7a56d315e40aedc47da1c65e8b231e3551c4234f
ezioruan <ezioruan@gmail.com>
2021-01-12 17:28:57 +0800

    update ci

--- tools/publish.sh
+++ tools/publish.sh
@@ -5,12 +5,9 @@ GIT_BRANCH=$(git symbolic-ref --short HEAD)
 GIT_VERSION="$(git rev-parse --short HEAD)"
 echo "publish version ${RELEASE_VERSION}"
 
-#git config --local user.name "ezioruan"
-#git config --local user.email "ezioruan@gmail.com"
-#
-#echo "publishing to npm..."
-#
-#npm run bundle
+git config --local user.name "ezioruan"
+git config --local user.email "ezioruan@gmail.com"
+npm run bundle
 
 git tag -a ${RELEASE_VERSION} -m "Release ${RELEASE_VERSION}. Revision is: ${GIT_VERSION}" | true
 git push origin ${RELEASE_VERSION} | true
